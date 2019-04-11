d67cc077050fe88fc7bc27e2f8cd1b7ab3ad4569
lecongdung <ledung276276@gmail.com>
2019-04-11 17:12:14 +0700

    add primary key

---
+++ console.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users
+(
+  user_id  INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(40),
+  password VARCHAR(255),
+  email    VARCHAR(255)
+);
+
+SELECT *
+FROM users;
+
+CREATE TABLE roles(
+  role_id INT AUTO_INCREMENT,
+  role_name VARCHAR(50),
+  PRIMARY KEY (role_id)
+);
+
+CREATE TABLE userroles(
+  user_id INT NOT NULL ,
+  role_id INT NOT NULL ,
+  PRIMARY KEY (user_id,role_id),
+  FOREIGN KEY (user_id)REFERENCES users(user_id),
+  FOREIGN KEY (role_id)REFERENCES roles(role_id)
+);
+
+SELECT * FROM userroles;
